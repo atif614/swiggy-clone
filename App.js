d2b2a6eee996d47480bab41af25f8e569e80314f
atif614 <razaatif20786@gmail.com>
2024-03-19 00:15:04 +0530

    created header

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-const HeadingComponent = () => (
-   <div>
-    <h1>Namaste React Functional Component</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+       <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ8JIwobDv1bDxu1v2Nver2VKYMxm3zdU_wg&usqp=CAU" />
+      </div>
+      <div className="nav-items"> 
+         <ul>
+          <li>Home</li> 
+          <li>About Us</li>         
+          <li>Contact Us</li>          
+          <li>Cart</li>
+         </ul>
+      </div>
+    </div>
+  )
+}
+
+const App = () => (
+  <div className="app">
+    <Header/>
   </div>
 )
 
-
-
- const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
